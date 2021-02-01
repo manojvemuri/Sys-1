af6688ee5103606b5679102d5a5c08335f53e267
Manoj Vemuri <37629940+manojvemuri@users.noreply.github.com>
2021-01-31 23:13:36 -0600

    Lecture files

---
+++ flow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int i,x;
+
+  x=0;
+  for (i=0; i<5; i++){
+    if (i%2 == 0)
+      continue;
+    x=x-i;
+    if (i%4 == 0)
+      break;
+    printf("%d\n",x);
+  }
+}
+
